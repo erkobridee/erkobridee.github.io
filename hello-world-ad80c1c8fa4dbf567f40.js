9aec3f1569f38351bef2ba7eaa0ca5f5f7d48b12
erkobridee <erkobridee@users.noreply.github.com>
2024-06-30 15:34:19 +0000

    deploy: 74ea85441a8b1f16bb55b17ab1b8b6d45803ff63

--- hello-world-ad80c1c8fa4dbf567f40.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{ACvR:function(o){o.exports=JSON.parse('{"message":"hello world"}')}}]);
-//# sourceMappingURL=hello-world-ad80c1c8fa4dbf567f40.js.map
