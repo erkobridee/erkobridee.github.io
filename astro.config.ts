f299a2d84566d7a23a2c709beec30fd3f6ec2561
erkobridee <erko.bridee@gmail.com>
2026-03-26 12:31:00 +0100

    update(astro): config

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,23 @@
+import type { AstroUserConfig } from 'astro';
+import type { Options as FaviconOptions } from 'astro-favicons';
+
+import { readFile } from 'fs/promises';
+
 // https://docs.astro.build/en/reference/configuration-reference/
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+
 // https://github.com/ACP-CODE/astro-favicons
 import favicons from 'astro-favicons';
+
 // https://github.com/natemoo-re/astro-icon
 import icon from 'astro-icon';
 
 // https://tailwindcss.com/docs/installation/framework-guides/astro
 import tailwindcss from '@tailwindcss/vite';
+
+//----------------------------------------------------------------------------//
 
 const integrations = [
   mdx(),
@@ -19,17 +28,19 @@ const integrations = [
 
 // https://docs.astro.build/en/guides/environment-variables/#default-environment-variables
 if (import.meta.env.PROD) {
-  integrations.push(
-    favicons({
-      masterPicture: './src/assets/images/erko_avatar_512.png',
-      emitAssets: true,
-      faviconsDarkMode: false
-    })
-  );
+  const options: FaviconOptions = {
+    name: 'Erko Bridee landing page',
+    short_name: 'Erko Bridee lp',
+    input: [
+      'public/favicon.svg',
+      await readFile('./src/assets/images/erko_avatar_512.png')
+    ]
+  };
+
+  integrations.push(favicons(options));
 }
 
-/** @type {import('astro').AstroUserConfig} */
-const config = {
+const config: AstroUserConfig = {
   integrations,
   vite: {
     plugins: [tailwindcss()]
